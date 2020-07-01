fa446a445dd5fb0df347b67b810e22f345ee6ba0
Jamie vullo <jvullo@protonmail.com>
2020-07-01 17:05:36 -0400

    Added function for eventually displaying time and creating a neew date instance.

---
+++ app.js
@@ -0,0 +1,13 @@
+
+
+function displayTime() {
+    // new date instance
+    var newDate = new Date();
+
+    var hours = newDate.getHours();
+    var minutes = newDate.getMinutes();
+    var seconds = newDate.getSeconds();
+    var day = newDate.getDate();
+    var month = newDate.getMonth();
+    var year = newDate.getFullYear();
+}
