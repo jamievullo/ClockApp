66e46639a1fef98deba8ef76add8e7b816ac5a94
Jamie vullo <jvullo@protonmail.com>
2020-07-02 13:00:42 -0400

    Added ternarys for clock and dates adding zeros

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
-
+document.addEventListener('DOMContentLoaded', function () {
+    displayTime()
+});
 
 function displayTime() {
     // new date instance
-    var newDate = new Date();
+    
+    let newDate = new Date();
+    // need to check why month is coming in minus one(showing june in july)
+    let hours = newDate.getHours();
+    let minutes = newDate.getMinutes();
+    let seconds = newDate.getSeconds();
+    let day = newDate.getDate();
+    let month = newDate.getMonth() + 1;
+    let year = newDate.getFullYear();
 
-    var hours = newDate.getHours();
-    var minutes = newDate.getMinutes();
-    var seconds = newDate.getSeconds();
-    var day = newDate.getDate();
-    var month = newDate.getMonth();
-    var year = newDate.getFullYear();
+    hours = hours < 10 ? "0" + hours : hours
+    minutes = minutes < 10 ? "0" + minutes : minutes
+    seconds = seconds < 10 ? "0" + seconds : seconds
+    day = day < 10 ? "0" + day : day
+    month = month < 10 ? "0" + month : month
+    
+    let amOrPm = hours < "12" ? "am" : "pm"
+
+    let clockDisplay = document.getElementById("clock-display")
+    clockDisplay.innerHTML = `${month}-${day}-${year} - ${hours}:${minutes}:${seconds} ${amOrPm}`
 }
+
