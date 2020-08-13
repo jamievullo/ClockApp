f28182ccb7107b661cab605a5e88578352b47ae9
Jamie vullo <jvullo@protonmail.com>
2020-08-13 17:10:30 -0400

    Refactored a bunch

--- app.js
+++ app.js
@@ -1,50 +1,49 @@
+let isMilitaryTime = true
+let intervalID;
+
 document.addEventListener('DOMContentLoaded', function () {
-    setInterval(() => displayTime(), 1000)
+    displayTime()
+    setInterval(displayTime, 1000)
 });
-let twentyFour = true
+
 let button = document.getElementById("twelve-twentyfour")
-button.addEventListener("click", () => changeFormat())
+button.addEventListener("click", (e) => {
+    isMilitaryTime = !isMilitaryTime
+    
+    clearInterval(intervalID)
+    displayTime()
+    intervalID = setInterval(displayTime, 1000)
+
+
+    button.innerHTML = "Change Time Format"
+})
 
 
 function displayTime() {
-    // new date instance    
-    let newDate = new Date();
-    let hours = newDate.getHours();
-    let minutes = newDate.getMinutes();
-    let seconds = newDate.getSeconds();
-    let day = newDate.getDate();
-    let month = newDate.getMonth() + 1;
-    let year = newDate.getFullYear();
+    let newDate = new Date()
 
-    hours = hours < 10 ? "0" + hours : hours
-    minutes = minutes < 10 ? "0" + minutes : minutes
-    seconds = seconds < 10 ? "0" + seconds : seconds
-    day = day < 10 ? "0" + day : day
-    month = month < 10 ? "0" + month : month
+    const hours = formatTime(newDate.getHours())
+    const minutes = formatTime(newDate.getMinutes())
+    const seconds = formatTime(newDate.getSeconds())
+    const day = formatTime(newDate.getDate())
+    const month = formatTime(newDate.getMonth() + 1)
+    const year = formatTime(newDate.getFullYear())
 
-    formatTime(hours, minutes, seconds, day, month, year)
-}
+    let formattedHour = isMilitaryTime ? hours : getStandardHours(hours)
 
-function changeFormat() {
-    console.log(twentyFour)
-
-    if(twentyFour === true) {
-        twentyFour = false
-    } else if(twentyFour === false) {
-        twentyFour = true
-    }
-
-    button.innerHTML = "Change Time Format"
-}
-
-function formatTime(hours, minutes, seconds, day, month, year) {
-
-    let amOrPm = hours < "12" ? "am" : "pm"
-    let formattedHour = hours > "12" ? hours - 12 : hours
     let clockDisplay = document.getElementById("clock-display")
-    if(twentyFour === false) {
-        clockDisplay.innerHTML = `${month}-${day}-${year} - ${formattedHour}:${minutes}:${seconds} ${amOrPm}`
-    } else {
-        clockDisplay.innerHTML = `${month}-${day}-${year} - ${hours}:${minutes}:${seconds}`
-    }
+
+    clockDisplay.innerHTML = `${month}-${day}-${year} - ${formattedHour}:${minutes}:${seconds} ${getAmPm()}`
+}
+
+function formatTime(time) {
+    return time < 10 ? "0" + time : time
+}
+
+function getStandardHours(hours) {
+    return parseInt(hours) > 12 ? parseInt(hours) - 12 : hours
+}
+
+function getAmPm(hours) {
+    return isMilitaryTime ? "" : (parseInt(hours) < 12 ? "am" : "pm")
 }
